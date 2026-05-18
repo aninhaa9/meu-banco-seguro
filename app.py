860a314bfe10fc964fdf52b0c296fc52c9031a42
Ana Carolyna Da Silva <00001113965174sp@aluno.educacao.sp.gov.br>
2026-05-18 17:25:11 -0300

    Bug inesperado

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-import os
-
-senha_do_banco = os.genetv('SENHA_BANCO', 'NAO_CONFIGURADA')
-
-print("--- SISTEMA DO BANCO DIGITAL ---")
-
-if senha_do_banco == '12345':
-    print("SENHA CORRETA!, acesso liberado ao cofre.")
-else:
-    print(" ACESSO NEGADO!, cahbe de segurança nao encontrada.")
+print("ERRO CRITICO! SISTEMA FORA DO AR")
+exit(1)
